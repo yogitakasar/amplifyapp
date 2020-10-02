2fb9b746113389cb59145cea7c362e0e7dc79304
yogitakasar <yogitakasar156@gmail.com>
2020-10-02 16:10:33 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React yup 
+
+          redeployed
         </a>
       </header>
     </div>
